0e40fabbdb7b2077090bc5d1c6a7855cce819193
Adrien Aucher <aucher.adrien@gmail.com>
2018-12-15 13:59:33 +0000

    frontend: add a Gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      js: {
+        src: [
+          'resources/js/*.js',
+        ],
+        dest: 'public/js/<%= pkg.name %>.js',
+      },
+    },
+    concat: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      // js: {
+      //   src: [
+      //     'node_modules/lodash/lodash.js',
+      //     'resources/js/*.js',
+      //   ],
+      //   dest: 'public/js/<%= pkg.name %>.js'
+      // },
+      css: {
+        src: ['resources/css/*.css'],
+        dest: 'public/css/<%= pkg.name %>.css'
+      },
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'public/js/<%= pkg.name %>.min.js': ['<%= browserify.js.dest %>']
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'public/css/<%= pkg.name %>.min.css': ['<%= concat.css.dest %>'],
+        }
+      }
+    },
+    jshint: {
+      // Define the files to lint:
+      files: ['Gruntfile.js', 'resources/**/*.js', 'tests/**/*.js'],
+      // Configure JSHint (documented at http://www.jshint.com/docs/):
+      options: {
+        // More options here if you want to override JSHint defaults:
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'browserify', 'concat', 'uglify']);
+};
+
+/* vim: set ts=2 sw=2: */
