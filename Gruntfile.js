f1a4513bac0a28f91679cad75cb3a51359e2d197
Adrien Aucher <aucher.adrien@gmail.com>
2018-12-15 15:31:03 +0000

    frontend: add cssmin task to default task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,7 +76,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint', 'browserify', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'browserify', 'concat', 'uglify', 'cssmin']);
 };
 
 /* vim: set ts=2 sw=2: */
