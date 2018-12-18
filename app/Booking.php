52171249131e556f820f7d990beafa5bc80cb8ff
Adrien Aucher <aucher.adrien@gmail.com>
2018-12-18 01:56:26 +0100

    backend/booking: implement Booking::checkUserCollision().

--- app/Booking.php
+++ app/Booking.php
@@ -23,23 +23,47 @@ class Booking extends Model
         return parent::save($options);
     }
 
-    public function checkRoomCollision() {
+    /**
+     * checkCollision return true if this meeting overlaps or surrounds
+     * an other booking.
+     * Support room collision and user collision.
+     *
+     * @param $column is either room_id or user_id.
+     * @param $value is the room_id or user_id to check for collision.
+     */
+    protected function checkCollision($column, $value) {
         return DB::table('bookings')
             ->where([
-                ['room_id', $this->room_id],
+                [$column, $value],
                 ['start', '<=', $this->start],
                 ['end', '>', $this->start],
             ])
             ->orWhere([
-                ['room_id', $this->room_id],
+                [$column, $value],
                 ['start', '<', $this->end],
                 ['end', '>=', $this->end],
             ])
             ->orWhere([
-                ['room_id', $this->room_id],
+                [$column, $value],
                 ['start', '>=', $this->start],
                 ['end', '<', $this->end],
             ])
             ->limit(1)->exists();
+    }
+
+    /**
+     * checkRoomCollision returns true is this room is already booked at
+     * the same time.
+     */
+    public function checkRoomCollision() {
+        return $this->checkCollision('room_id', $this->room_id);
+    }
+
+    /**
+     * checkUserCollision returns true if the user who books this booking has
+     * an other booking at the same time.
+     */
+    public function checkUserCollision() {
+        return $this->checkCollision('user_id', $this->user_id);
     }
 }
