f2004dac00094e8c176f2861bcd3144f5b97ab57
Adrien Aucher <aucher.adrien@gmail.com>
2018-12-18 01:03:36 +0100

    backend/booking: implement Booking::checkRoomCollision().

--- app/Booking.php
+++ app/Booking.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Booking extends Model
 {
@@ -16,6 +17,29 @@ class Booking extends Model
         if ($this->end <= $this->start) {
             return false;
         }
+        if ($this->checkRoomCollision()) {
+            return false;
+        }
         return parent::save($options);
+    }
+
+    public function checkRoomCollision() {
+        return DB::table('bookings')
+            ->where([
+                ['room_id', $this->room_id],
+                ['start', '<=', $this->start],
+                ['end', '>', $this->start],
+            ])
+            ->orWhere([
+                ['room_id', $this->room_id],
+                ['start', '<', $this->end],
+                ['end', '>=', $this->end],
+            ])
+            ->orWhere([
+                ['room_id', $this->room_id],
+                ['start', '>=', $this->start],
+                ['end', '<', $this->end],
+            ])
+            ->limit(1)->exists();
     }
 }
