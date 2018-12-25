65370df06b710340e6ffc770707fa93dc60d6827
Adrien Aucher <aucher.adrien@gmail.com>
2018-12-25 23:47:15 +0100

    frontend: use Font Awesome CDN.

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -6,6 +6,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'clerk') }}</title>
         <link rel="stylesheet" href="css/app.css">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/fontawesome.css" integrity="sha384-jLuaxTTBR42U2qJ/pm4JRouHkEDHkVqH0T1nyQXn1mZ7Snycpf6Rl25VBNthU4z0" crossorigin="anonymous">
     </head>
     <body>
         <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
