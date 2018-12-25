d83f040b3e1d3fe9d3afb4aee77140cf3b0f1d92
Adrien Aucher <aucher.adrien@gmail.com>
2018-12-25 19:28:48 +0100

    backend/booking: document StoreBooking request.

--- app/Http/Requests/StoreBooking.php
+++ app/Http/Requests/StoreBooking.php
@@ -52,29 +52,42 @@ class StoreBooking extends FormRequest
         ];
     }
 
+    /**
+     * validateRoomCollision checks if the request will produce a room collision.
+     *
+     * @throws ValidationException with 109 status
+     */
     public function validateRoomCollision(Validator $validator, Booking $booking)
     {
         if ($booking->checkRoomCollision()) {
             $validator->errors()->add(
-                'data.attributes.room.data.id', 'This room is already in use at that time.'
-            );
-            throw (new ValidationException($validator))->status(409);
-        }
-    }
-
-    public function validateUserCollision(Validator $validator, Booking $booking)
-    {
-        $override = $this->input('meta.overrideUserCollision');
-        if (!$override && $booking->checkUserCollision()) {
-            $validator->errors()->add(
-                'data.attributes.start', 'You already have a booking at that time. You may override this warning.'
+                'data.attributes.room.data.id',
+                'This :attribute is already in use at that time.'
             );
             throw (new ValidationException($validator))->status(409);
         }
     }
 
     /**
-    * Configure the validator instance.
+     * validateUserCollision checks if the request will produce a user collision.
+     * This test is bypassed if meta.overrideUserCollision is set to true in the request.
+     *
+     * @throws ValidationException with 109 status
+     */
+    public function validateUserCollision(Validator $validator, Booking $booking)
+    {
+        $override = $this->input('meta.overrideUserCollision');
+        if (!$override && $booking->checkUserCollision()) {
+            $validator->errors()->add(
+                'data.attributes.start',
+                'You already have a booking at that time. You may override this warning.'
+            );
+            throw (new ValidationException($validator))->status(409);
+        }
+    }
+
+    /**
+    * Add room & user collision checks in the validation process.
     *
     * @param  \Illuminate\Validation\Validator  $validator
     * @return void
@@ -91,10 +104,10 @@ class StoreBooking extends FormRequest
             $booking->fill([
                 'user_id' => Auth::id(),
                 'room_id' => $this->input('data.relationships.room.data.id'),
-                'start' => $start,
-                'end'   => (clone $start)->addMinutes(
-                    $this->input('data.attributes.duration')
-                ),
+                'start'   => $start,
+                'end'     => (clone $start)->addMinutes(
+                                 $this->input('data.attributes.duration')
+                             ),
             ]);
 
             $this->validateRoomCollision($validator, $booking);
