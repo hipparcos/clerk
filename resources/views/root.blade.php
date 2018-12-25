159c6994649fac5a448864bbb478406e3343c13b
Adrien Aucher <aucher.adrien@gmail.com>
2018-12-25 20:09:37 +0100

    frontend: move layout content to root template.

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -1,12 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="has-navbar-fixed-top">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'clerk') }}</title>
+        <link rel="stylesheet" href="css/clerk.css">
     </head>
     <body>
-        <h1>{{ config('app.name', 'clerk') }}</h1>
+        <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/">
+                    <i class="fas fa-building"></i>&nbsp;{{ config('app.name', 'clerk') }}
+                </a>
+            </div>
+
+            <div id="navbar" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                        Home
+                    </a>
+                </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a class="button is-primary">
+                                <strong>Register</strong>
+                            </a>
+                            <a class="button is-light">
+                                Log in
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+        </div>
+
+        <script src="js/clerk.js"></script>
     </body>
 </html>
