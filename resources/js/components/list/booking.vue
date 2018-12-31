29444d4e756a60eace775f53e8968a5bed93e590
Adrien Aucher <aucher.adrien@gmail.com>
2018-12-31 11:42:37 +0100

    frontend/booking: add date selection in list view.

--- resources/js/components/list/booking.vue
+++ resources/js/components/list/booking.vue
@@ -1,51 +1,91 @@
 <template>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Room</th>
-                <th>Start</th>
-                <th>Duration</th>
-                <th>User</th>
-                <th>Email</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <booking-tr
-                v-for="booking in bookings"
-                :key="booking.id"
-                :booking="booking"
-                :token="token"
-                @booking-update="onUpdate"
-                @booking-delete="onDelete"
-                >
-            </booking-tr>
-        </tbody>
-    </table>
+    <div>
+        <div class="field is-horizontal">
+            <div class="field-label is-normal">
+                <label class="label">Date</label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <p class="control">
+                        <date-picker type="date" lang="en" format="DD-MM-YYYY"
+                            v-model="selectedDate"
+                            ></date-picker>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Room</th>
+                    <th>Start</th>
+                    <th>Duration</th>
+                    <th>User</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <booking-tr
+                    v-for="booking in bookings"
+                    :key="booking.id"
+                    :booking="booking"
+                    :token="token"
+                    @booking-update="onUpdate"
+                    @booking-delete="onDelete"
+                    >
+                </booking-tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
+import _ from 'lodash'
 import axios from 'axios'
+import moment from 'moment'
+import DatePicker from 'vue2-datepicker'
 import bookingTrComponent from './booking-tr.vue'
 
 export default {
-    components: { 'booking-tr': bookingTrComponent },
+    components: {
+        'booking-tr': bookingTrComponent,
+        DatePicker,
+    },
     props: {
         token: String,
     },
     created: function() {
         this.getBookings()
+        this.debounceGetBookings = _.debounce(this.getBookings, 500)
     },
     data: function() {
         return {
+            date: moment(),
             bookings: [],
         }
+    },
+    computed: {
+        selectedDate: {
+            get: function() {
+                return this.date
+            },
+            set: function(d) {
+                this.date = moment(d)
+            },
+        }
+    },
+    watch: {
+        date: function(oldVal, newVal) {
+            this.debounceGetBookings()
+        },
     },
     methods: {
         getBookings: function() {
             axios({
                 method: 'get',
-                url: '/api/bookings',
+                url: '/api/bookings/' +
+                    this.date.format('YYYY/MM/DD'),
                 headers: {
                     'Authorization': 'Bearer ' + this.token,
                 },
