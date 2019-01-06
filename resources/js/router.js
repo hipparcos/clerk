f157f5504d801c7036a4c3c02eec5082020cd5af
Adrien Aucher <aucher.adrien@gmail.com>
2019-01-06 23:44:25 +0100

    frontend: fix redirect to /bookings/agenda from /.

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,15 +3,24 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-const root = { template: '<p></p>' }
-
-const routes = [
-    { path: '/', redirect: '/bookings/agenda' },
-]
-
 import auth from './auth/routes.js'
 import user from './user/routes.js'
 import booking from './booking/routes.js'
+
+import authStore from './auth/store.js'
+
+const routes = [
+    {
+        path: '/',
+        beforeEnter: function(to, from, next) {
+            if (authStore.getters.isAuthenticated(authStore.state)) {
+                next('/bookings/agenda')
+                return
+            }
+            next('/login')
+        },
+    },
+]
 
 export default new VueRouter({
     mode: 'history',
