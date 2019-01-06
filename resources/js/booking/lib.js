ed538c19b0526184e24f4ca8737a829ddf786a4a
Adrien Aucher <aucher.adrien@gmail.com>
2019-01-06 17:23:55 +0100

    frontend/booking: add sorters (compareFunction) to lib.js.

--- resources/js/booking/lib.js
+++ resources/js/booking/lib.js
@@ -12,10 +12,30 @@ const nextSlot = () => {
     return nextHour.subtract(diffSlot * slot, unit)
 }
 
+const roomSorter = (l, r) => {
+    let lroom = l.room.name
+    let rroom = r.room.name
+    return lroom.localeCompare(rroom)
+}
+
+const userSorter = (l, r) => {
+    let luser = l.user.name
+    let ruser = r.user.name
+    return luser.localeCompare(ruser)
+}
+
+const timeSorter = (l, r) => {
+    return l.start.isBefore(r.start) ? -1 : 1
+}
+
 export default {
     // const
     slot,
     unit,
     // func
     nextSlot,
+    // compareFunctions
+    roomSorter,
+    userSorter,
+    timeSorter,
 }
